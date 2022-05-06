8434a917ccecff48dadd93a2fa3f40acb6b1560e
iamkhaihoang <khai.hoang@outlook.com>
2022-05-06 01:48:54 -0400

    Update Java code

--- PROJECT-JENKINS-ECR/project_app.java
+++ PROJECT-JENKINS-ECR/project_app.java
@@ -4,12 +4,12 @@ public static void main(String[] args)
 	{           
 		System.out.print("Enter Firstname: ");   
 		//reading a string from the user  
-		String firstname = "";
-		firstname = System.console().readLine();  
+		//String firstname = System.console().readLine();  
+		String firstname = "Khai";
 		System.out.print("Enter Lastname: ");   
 		//reading a string from the user  
-		String lastname = "";
-		lastname = System.console().readLine();  
+		//String lastname = System.console().readLine();  
+		String lastname = "Hoang";
 		String strhello = "Welcome and Hello " +firstname + " " + lastname;
         strhello = strhello + " to Ziyotek DEVOPS16 team.";
 		System.out.println(strhello);   
