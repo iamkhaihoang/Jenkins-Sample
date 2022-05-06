753c54add5e03676b7e108ea5de52d55a506b04f
iamkhaihoang <khai.hoang@outlook.com>
2022-05-06 01:24:50 -0400

    Add Jenkinsfile-2ERC

--- PROJECT-JENKINS-ECR/project_app.java
+++ PROJECT-JENKINS-ECR/project_app.java
@@ -4,10 +4,12 @@ public static void main(String[] args)
 	{           
 		System.out.print("Enter Firstname: ");   
 		//reading a string from the user  
-		String firstname = System.console().readLine();  
+		String firstname = "";
+		firstname = System.console().readLine();  
 		System.out.print("Enter Lastname: ");   
 		//reading a string from the user  
-		String lastname = System.console().readLine();  
+		String lastname = "";
+		lastname = System.console().readLine();  
 		String strhello = "Welcome and Hello " +firstname + " " + lastname;
         strhello = strhello + " to Ziyotek DEVOPS16 team.";
 		System.out.println(strhello);   
